5aaac9c9b28915635571d6368342a9314bdcae09
Hcat <admin@hcat.work>
2022-02-18 19:03:07 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:AdminHcat/Minecraft.git master:gh-pages
+git push -f git@github.com:AdminHcat/MinecraftServer.git master:gh-pages
 
 cd -
